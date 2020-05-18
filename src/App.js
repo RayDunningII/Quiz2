e9f3d2399cb09bfce4936cc358b8b72f2036191e
Ray Dunning <raydunning@Rays-Air.lan>
2020-05-17 20:30:41 -0500

    can answer questions and get correct or not

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Axios from "axios";
 import React, { useState, useEffect } from 'react';
-
+import Ray from './Ray'
 const App = () => {
     const [movie, setMovie] = useState('');
     const [answers, setAnswers] = useState('');
     const [question, setQuestion] = useState('');
     const [correct, setCorrect] = useState('');
-    const [data, setData] = useState([ ])
+    const [data, setData] = useState([]);
     const [value, setValue] = useState('')
     const index = 0 
     const score = 0
@@ -16,35 +16,52 @@ const App = () => {
     useEffect(() => {
         // let questions = data.movie 
        Axios.get('http://localhost:4000/')
-       .then(response => setData(response.data))
-       setMovie(data.movie) //[{}]
-       setCorrect(data.correct)
-      
-    },[data.movie,data.correct])
+       .then(response => {
+        setData(response.data);
+        // setCorrect(response.data.correct)
+ 
+        // setMovie(response.data.movie);
+    }) 
 
-    const checkAnswer = (value,correctAns) => {
-        if(value === correctAns){
+},[])
+  console.log(data)
+  
+
+    const checkAnswer = (e) => {
+        let userResponse = Number(e.target.id)
+        console.log(userResponse, data[0].correct)
+        if(userResponse === data[0].correct){
             window.alert("correct")
-            index += 1
-            score += 1
-            setAnswers(data.answer[index])
-            setCorrect(data.correct[index])
-            setMovie(data.movie[index])
+            // index += 1
+            // score += 1
+            // setAnswers(data.answer[index])
+            // setCorrect(data.correct[index])
+            // setMovie(data.movie[index])
         } else {
             window.alert('try again')
         }
+    }
+    if(data.length === 0){
+        return null;
     }
     return(
         <div className="container">
         <div className="row">
           <div className="col-lg-10 col-lg-offset-1">
             <div id="question"> 
-            <p key={index}>{movie[0]}</p>
+            {data[0].movie} 
+            <Ray quiz={data}/> 
+            <p key={index}>{data.movie}</p>
             </div>
-             <button   value={answers[0]} id='one' onClick={() => checkAnswer(value,correct)}>{data.answers}</button>
-            <button value={answers[0]} id='two' onClick={() => checkAnswer(value,correct)}>{answers[1]}</button>
-            <button value={answers[0]} id='three'onClick={() => checkAnswer(value,correct)}>{answers[2]}</button>
-            <button value={answers[0]} id='four' onClick={() => checkAnswer(value,correct)}>{answers[3]}</button>
+            <div>
+        
+                 <div>
+                 <button  className='one' id={1} onClick={(e) => checkAnswer(e,correct)} >{data[0].answers[0]}</button>
+                 <button  className='two' id={2} onClick={(e) => checkAnswer(e,correct)}>{data[0].answers[1]}</button>
+                 <button  className='three'id={3}onClick={(e) => checkAnswer(e,correct)}>{data[0].answers[2]}</button>
+                 <button  className='four' id={4} onClick={(e) => checkAnswer(e,correct)}>{data[0].answers[3]}</button>
+                 </div>
+            </div>
         </div>
         </div>
         </div>
---
+++ src/Ray.js
@@ -0,0 +1,15 @@
+import Axios from "axios";
+import React, { useState, useEffect } from 'react';
+
+const Ray = (props) => {
+   console.log(props.quiz)
+   //do math random from 0 to length of props.quiz and display
+   //if user chooses correct question
+   
+
+
+
+}
+
+
+export default Ray
